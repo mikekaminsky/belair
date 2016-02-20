3f84c23801ee45674b4e7735f9cc440adaf139f6
Michael Kaminsky <michaelkaminsky@Michaels-MacBook-Air.local>
2016-02-20 13:58:12 -0500

    hook up shows index

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,5 +1,6 @@
-belAir.controller('HomeController', ['$scope', function($scope) {
-    console.log("app controller");
-
-    $scope.message = 'Hello Radio!'
+belAir.controller('HomeController', ['$http', '$scope',
+  function($http, $scope) {
+    $http.get('/shows').then(function (response) {
+      $scope.shows = response.data;
+    });
 }]);
