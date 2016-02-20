c133ded02b980ff27df389c6b79298053e7bb78f
Michael Kaminsky <michaelkaminsky@Michaels-MacBook-Air.local>
2016-02-20 17:40:24 -0500

    give feedback on update

--- app/assets/javascripts/controllers/admin_show_controller.js
+++ app/assets/javascripts/controllers/admin_show_controller.js
@@ -5,11 +5,17 @@ belAir.controller('AdminShowController', ['Shows', '$location', '$routeParams', 
       if (response.data.show) {
         $scope.show = response.data.show;
       } else {
-        $location.path('/').replace();
+        $location.path('/admin').replace();
       }
     });
 
     $scope.saveShow = function saveShow() {
-      Shows.update(id, $scope.show);
+      Shows.update(id, $scope.show).then(function (response) {
+        if (response.data.errors) {
+          $scope.errors = response.data.errors
+        } else {
+          $location.path('/admin').replace();
+        }
+      });
     }
 }]);
