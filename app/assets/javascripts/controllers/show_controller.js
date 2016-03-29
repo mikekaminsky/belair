d9b0da1f59c0ebe25a5724219a07345a79b7b844
Steve Ellis <email@steveell.is>
2016-03-28 21:50:44 -0400

    move audio controlls into a service

--- app/assets/javascripts/controllers/show_controller.js
+++ app/assets/javascripts/controllers/show_controller.js
@@ -1,5 +1,5 @@
-belAir.controller('ShowController', ['Shows', '$location', '$routeParams', '$sce', '$scope',
-  function(Shows, $location, $routeParams, $sce, $scope) {
+belAir.controller('ShowController', ['AudioPlayer', 'Shows', '$location', '$routeParams', '$sce', '$scope',
+  function(AudioPlayer, Shows, $location, $routeParams, $sce, $scope) {
 
   Shows.show($routeParams.id).then(function (response) {
     if (response.data.show) {
@@ -9,14 +9,8 @@ belAir.controller('ShowController', ['Shows', '$location', '$routeParams', '$sce
     }
   });
 
-  $scope.encoded = function encoded(url) {
-    return $sce.trustAsResourceUrl(encodeURI(url));
-  };
-
   $scope.play = function play(episode) {
-    var audio = $(audioJS.element);
-    audio.attr('src', episode.file_url);
-    audioJS.play();
+    AudioPlayer.play(episode.file_url);
   };
 
 }]);
