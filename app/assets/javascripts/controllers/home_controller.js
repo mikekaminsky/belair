034c8189ba75d62fde1f7dc64a51e90d8689bf39
idbentley <ian.bentley@gmail.com>
2016-04-26 21:13:52 -0400

    Change to mediaelement, and add livestream.

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,7 +1,12 @@
-belAir.controller('HomeController', ['Shows', '$scope', function(Shows, $scope) {
+belAir.controller('HomeController', ['Shows', '$scope', '$http', 'AudioPlayer', function(Shows, $scope, $http, AudioPlayer) {
     Shows.index().then(function (response) {
       $scope.shows = response.data.shows;
 
       console.log($scope.shows);
     });
+
+  $scope.play = function play() {
+		var liveStreamUrl = "http://s2.voscast.com:9106/;stream.nsv";
+		AudioPlayer.play(liveStreamUrl);
+	}
 }]);
