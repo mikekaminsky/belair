639cfa7f5efca5bc3c142700819f1e2c14242b28
Steve Ellis <email@steveell.is>
2016-05-03 00:15:39 -0400

    Catch audioplayer error

--- app/assets/javascripts/services/audio_player.js
+++ app/assets/javascripts/services/audio_player.js
@@ -1,9 +1,17 @@
 belAir.service('AudioPlayer', [function AudioPlayerService() {
+  var nicecastError = "Failed to load because no supported source was found.";
+
   this.player = new MediaElementPlayer('#audio-player', {});
 
   this.play = function play(url) {
     this.player.setSrc(url);
-    this.player.play();
+    this.player.load();
+
+    this.player.media.play().catch(function (error) {
+      if (error.message != nicecastError) {
+        console.error(error.message);
+      }
+    })
   }
 
 }]);
