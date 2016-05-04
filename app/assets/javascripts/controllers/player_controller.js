77db38549e50467d210fd7e9701c1ce9c11a225a
Steve Ellis <email@steveell.is>
2016-05-03 22:20:56 -0400

    Player styling

--- app/assets/javascripts/controllers/player_controller.js
+++ app/assets/javascripts/controllers/player_controller.js
@@ -1,8 +1,17 @@
 belAir.controller('PlayerController', ['AudioPlayer', '$scope', function(AudioPlayer, $scope) {
-	AudioPlayer.autoPlay();
+  AudioPlayer.autoPlay();
 
-	$scope.play = function play() {
-		var liveStreamUrl = "http://s2.voscast.com:9106/;stream.nsv";
-		AudioPlayer.play(liveStreamUrl);
-	}
+  $scope.play = function play() {
+    var liveStreamUrl = "http://s2.voscast.com:9106/;stream.nsv";
+    AudioPlayer.play(liveStreamUrl);
+  }
+
+  $scope.showName = function showName() {
+    return AudioPlayer.episode.show;
+  }
+
+  $scope.episodeName = function episodeName() {
+    return AudioPlayer.episode.name;
+  }
+
 }])
