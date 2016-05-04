01b216f4bf832d3cb5f5620c54fc6563b0469ed3
Steve Ellis <email@steveell.is>
2016-05-03 21:24:46 -0400

    Move autoplay logic to frontend
    let audio tag figure out if an audio stream exists... better than
    rewriting that logic

--- app/assets/javascripts/services/audio_player.js
+++ app/assets/javascripts/services/audio_player.js
@@ -3,21 +3,27 @@ belAir.service('AudioPlayer', ['$http', function AudioPlayerService($http) {
   var player = new MediaElementPlayer('#audio-player', {});
   var voscastError = "Failed to load because no supported source was found.";
 
-  this.play = function play(url) {
+  this.play = function play(url, callback) {
     player.setSrc(url);
     player.load();
 
     player.media.play().catch(function (error) {
-      console.log("Error playing audio: " + url);
-      if (error.message != voscastError) {
-        console.error(error.message);
+      if (callback) {
+        callback();
+      } else {
+        console.log("Error playing audio: " + url);
+        if (error.message != voscastError) {
+          console.error(error.message);
+        }
       }
     })
   }
 
   this.autoPlay = function auotPlay(url) {
-    $http.get('/api/livestream').then(function (response) {
-      audio.play(response.data.episode.file_url);
+    audio.play(BelAir.livestreamURL, function () {
+      $http.get('/api/episodes/random').then(function (response) {
+        audio.play(response.data.episode.file_url);
+      });
     });
   }
 }]);
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'application#home'
 
   scope path: :api do
-    resources :episodes, only: [:create, :show, :update, :destroy]
+    resources :episodes, only: [:create, :show, :update, :destroy] do
+      collection do
+        get 'random'
+      end
+    end
     resources :shows, except: [:new]
     resources :s3_policies, only: [:new]
 
