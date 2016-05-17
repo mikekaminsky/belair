96b22d8f2af1789ff76aab22ede3a3c7840142ba
omnikrll <kfhohn@gmail.com>
2016-05-16 21:25:59 -0400

    include fonts. home page adheres much closer to designs now.

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,5 +1,5 @@
 belAir.controller('HomeController', ['Shows', '$scope', function(Shows, $scope) {
     Shows.index().then(function (response) {
-      $scope.shows = response.data.shows;
+      $scope.shows = response.data.shows.slice(0, 6);
     });
 }]);
