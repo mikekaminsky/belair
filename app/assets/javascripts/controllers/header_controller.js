36ee450acc01ccdcc4eb8cacbb4123ed722ea2f7
omnikrll <kfhohn@gmail.com>
2016-05-17 03:39:08 -0400

    side nav worksssssss

--- app/assets/javascripts/controllers/header_controller.js
+++ app/assets/javascripts/controllers/header_controller.js
@@ -1,3 +1,31 @@
-belAir.controller('HeaderController', ['$scope', function($scope) {
-	// do shit
+belAir.controller('HeaderController', ['$scope', '$aside', function($scope, $aside) {
+
+	$scope.asideState = {
+		open: false
+	};
+
+	$scope.openAside = function(position, backdrop) {
+		$scope.asideState = {
+			open: true,
+			position: position
+		};
+
+		function postClose() {
+			$scope.asideState.open = false;
+		}
+
+		$aside.open({
+			templateUrl: 'nav.html',
+			placement: position,
+			size: 'sm',
+			backdrop: backdrop,
+			controller: function($scope, $uibModalInstance) {
+				$scope.close = function(e) {
+					$uibModalInstance.dismiss();
+					e.stopPropagation();
+				};
+			}
+		}).result.then(postClose, postClose);
+	}
+
 }]);
