eb86c5fd658dff4e36d1eafca49f09220970aad8
Michael Kaminsky <kaminsky.michael@gmail.com>
2016-05-17 18:25:24 -0400

    Change js compression settings to keep heroku from breaking angular.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,11 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
+  
+  # Set this option according to this SO answer:
+  # http://stackoverflow.com/a/25404498/3066464
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
